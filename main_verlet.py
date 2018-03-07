579ef5f13ca01967abe900d5e3e65e40563c1ebb
lcombe01 <36709110+lcombe01@users.noreply.github.com>
2018-03-07 14:14:59 +0100

    Update main_verlet.py

--- main_verlet.py
+++ main_verlet.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 28 12:45:34 2018
-
 @author: sdiop
-
 Nouvelle version du main avec l'algorithme de verlet
 """
 
@@ -62,7 +60,11 @@ class grain :
         self.pos_a = np.array([x0,y0]) - np.array([vx0,vy0])*dt  # position à t-dt
         self.pos_n = np.array([x0,y0]) #position à t+dt
         self.vit_n = np.array([vx0,vy0]) #position à t+dt
-         
+        self.force = np.array([0,0]) #force_totale(i) a t 
+        self.force_voisins = np.array([0.,0.])
+        self.force_paroi = np.array([0.,0.])
+        self.grav = np.array([0.,0.])
+        
         self.voisins = [] #liste des voisins
         self.bords = [] #liste de booléens [gauche,bas,droite], True si contact, False sinon
         
@@ -171,7 +173,7 @@ def force_paroi(i):
     
     
 
-def force_totale(i, gravite = True, paroi = True):
+def force_totale(i):
     '''
     Fonction renvoyant la force totale s'appliquant sur le grain i.
     gravite : détermine si le poids est pris en compte dans la force
@@ -180,14 +182,21 @@ def force_totale(i, gravite = True, paroi = True):
     force = np.array([0.,0.],dtype='float64')
     i.detecter_voisins()
     i.detecter_paroi()
+    i.force_voisins = np.array([0.,0.])
+    i.force_paroi = np.array([0.,0.])
+    i.grav = np.array([0.,0.])
     for j in i.voisins:
-        force += force_grain(j,i)
-        force += force_grain(j,i)
+        fg = force_grain(j,i) 
+        force += fg
+        i.force_voisins += fg 
+        
     if paroi == True :
-        force += force_paroi(i)
-        force += force_paroi(i)
+        fp = force_paroi(i)  
+        force += fp 
+        i.force_paroi += fp
     if gravite == True :
-        force += np.array([0.,-m*g],dtype='float64')
+        fgrav = np.array([0.,-m*g],dtype='float64')
+        force += fgrav
     return force
     
 
@@ -203,7 +212,9 @@ def mise_a_jour(remove=True):
         lorsque remove vaut True : si un grain sort du silo (y < y_min) on ne le prend plus en compte dans l'étude
     '''
     for i in grains:
-        i.pos_n = 2 * i.pos - i.pos_a + 1/m * force_totale(i) * dt**2
+        f = force_totale(i)
+        i.force = f 
+        i.pos_n = 2 * i.pos - i.pos_a + 1/m * f * dt**2
         i.vit_n = (i.pos_n - i.pos_a)/(2*dt)
     for i in grains:
         i.pos, i.pos_a = i.pos_n, i.pos
@@ -213,3 +224,40 @@ def mise_a_jour(remove=True):
                 grains.remove(i)
                 
                 
+def trajectoires(grains, t_final):
+    '''
+    Renvoie les trajectoires [X,Y,VX,VY,temps] qui sont tous des arrays d'un ensemble de grains initial 'grains', entre les instants 0 et t_final
+    '''
+    t = 0
+    X = [[] for k in range(len(grains))]
+    Y = [[] for k in range(len(grains))]
+    VX = [[] for k in range(len(grains))]
+    VY = [[] for k in range(len(grains))]
+    FX = [[] for k in range(len(grains))] #forces totales en x sur les grains
+    FY = [[] for k in range(len(grains))]  
+    FpX = [[] for k in range(len(grains))]#forces de la paroi en y sur les grains 
+    FpY = [[] for k in range(len(grains))]
+    FvX = [[] for k in range(len(grains))]#forces des voisisns en x sur les grains
+    FvY = [[] for k in range(len(grains))]
+    G = [[] for k in range(len(grains))]
+    
+    temps = []
+    while t < t_final:
+        temps.append(t)
+        for i in range(len(grains)) :
+            X[i].append(grains[i].pos[0])
+            Y[i].append(grains[i].pos[1])
+            VX[i].append(grains[i].vit[0])
+            VY[i].append(grains[i].vit[1])
+            FX[i].append(grains[i].force[0])
+            FY[i].append(grains[i].force[1])
+            FpX[i].append(grains[i].force_paroi[0])
+            FpY[i].append(grains[i].force_paroi[1])
+            FvX[i].append(grains[i].force_voisins[0])
+            FvY[i].append(grains[i].force_voisins[1])
+            G[i].append(grains[i].grav[1])
+
+
+        mise_a_jour()
+        t += dt
+    return(np.array(temps),np.array(X),np.array(Y),np.array(VX),np.array(VY),np.array(FX),np.array(FY),np.array(FpX),np.array(FpY),np.array(FvX),np.array(FvY),np.array(G))
