e428d4d914007369e024d503ff4be8a504387254
sdiop1 <36761576+sdiop1@users.noreply.github.com>
2018-04-11 13:38:46 +0200

    Add files via upload

--- test_dissipation_collision_symetrique.py
+++ test_dissipation_collision_symetrique.py
@@ -8,72 +8,94 @@ Created on Wed Mar  7 18:13:06 2018
 
 # paramètres :
 
-# grains
+# exterieur
 R = 1
 m = 1
-
-# exterieur
-L=20
-e=2
-y_min = -R
-g = 1
+L=200000
+e=20000
+y_min = -900000
 
 # paramètres des contacts
-Kc = 1e4 * m*g/R
-restitution = 0.3
-Kd = restitution**2*Kc
-Kc_p = 1e4 * m*g/R
-restitution_p = 0.3
-Kd_p = restitution_p**2*Kc_p
-Kt = 10
+Kc = 1e4
+rest = 0.8
+Kd = rest**2 * Kc
+
+Kc_p = 1e4
+rest_p = 0.4
+Kd_p = rest_p**2 * Kc_p
+
+
+Kt = 0
 mu = 0.7
-mu_p = 0.5
-Kt_p = 7
-
-gamma = 300
-xi = 10
+Kt_p = 0
+mu_p = 0.7
 
 
-dt = 1e-4
+dt = 1e-3
+
 paroi = False
 gravite = False
-remove=False
+remove= False
+
+
 
 
 if __name__=='__main__':
-    
-
-    
-    grains = [grain(-1.01,8,0.051,0),grain(1.01,8,-0.051,0),grain(-1.1,4,5,0),grain(1.1,4,-5,0),grain(-2.1,0,10,0),grain(2.1,0,-10,0),grain(-1.1,-4,50,0),grain(1.1,-4,-50,0),grain(-1.1,-8,100,0),grain(1.1,-8,-100,0)]
-
-    T,X,Y,VX,VY,FX,FY,FpX,FpY,FvX,FvY,G = trajectoires(grains, 1)
+    m = 1
     
     #%%
-    for k in range(len(grains)//2):
-        plt.plot(T,VX[2*k]**2/VX[2*k][0]**2, label = 'Vx0={}'.format(VX[2*k][0]))   
-    plt.title("Ec relative en fonction du temps")
-    plt.show()
-    plt.legend()
-    plt.grid()
-    #%%
-    for k in range(len(grains)//2):
-        plt.scatter(VX[2*k][0],1-VX[2*k][-1]**2/VX[2*k][0]**2)   
+    
+    eps = 0
+    for k in range(1,40):
+        grains = []
+        v0 = 1e-6* (k+3)**4
+        grains.append(grain(-1.001,eps-10,v0,0))
+        grains.append(grain(1,-10,0,0))
         
-    plt.title("Perte relative d'Ec en fonction de la vitesse initiale")
-    
-
-    plt.legend()
+        T,X,Y,VX,VY = trajectoires(grains, 5)
+        
+        if (X[1][-1]-X[0][-1])**2+(Y[1][-1]-Y[0][-1])**2 < 4 : col = 'red'
+        else : col = 'blue'
+        E = (VX[0]**2 + VY[0]**2 + VX[1]**2 + VY[1]**2)/2 
+        E = E/E[0]
+        perte = 1-E[-1]
+        plt.scatter(VX[0][0], perte, color = col)   
+        
+    plt.title(r"Perte relative d'$E_{c}$ en fonction de la vitesse initiale")
+    plt.xlabel(r'$v_{0}$')
+    plt.ylabel(r'$\eta$')
     plt.grid()
     plt.show()
+    
+    
     #%%
-    grains = [grain(-1.01,8,0.051,0),grain(1.01,8,-0.051,0),grain(-1.1,4,5,0),grain(1.1,4,-5,0),grain(-2.1,0,10,0),grain(2.1,0,-10,0),grain(-1.1,-4,50,0),grain(1.1,-4,-50,0),grain(-1.1,-8,100,0),grain(1.1,-8,-100,0)]
+    eps = 0
+    v0 = 1
+    y0 = 1e6
+    x0 = 1e6
+    grains = [grain(-1.1+x0,eps+y0,v0,0), grain(1+x0,0+y0,0,0)]
     
     fig = plt.figure(1,figsize=(6,6))
     plt.axis("equal")
-    ax = plt.axes(xlim=(-10,10), ylim=(-10,10))
+    ax = plt.axes(xlim=(-5+x0,5+x0), ylim=(-5+y0,5+y0))
     fenetre = fig, ax
     ani = animer(grains)
     plt.show()
     
+    #%%
+    eps = 0
+    v0 = 1
     
+    for n in range(10):
+        y0 = 10**(n-7)
+        x0 = 10**(n-7)
+        grains = [grain(-1.1+x0,eps+y0,v0,0), grain(1+x0,0+y0,0,0)]
+        T,X,Y,VX,VY = trajectoires(grains, 5)
+        E = (VX[0]**2 + VY[0]**2 + VX[1]**2 + VY[1]**2)/2 
+        E = E/E[0]
+    #    plt.plot(E)
+    #    plt.show()
+        plt.scatter(y0,VY[1][-1])
+        plt.xscale('log')
+    plt.show()
     
